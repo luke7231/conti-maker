14e072e746ce21d008010b079abeb11ada289ee3
luke7231 <tmddnjs7231@gmail.com>
2023-12-14 16:25:26 -0800

    feat: layout component applying

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,12 @@
 import React from "react";
 import Router from "./router";
-import styled from "styled-components";
-import { getResponsiveMaxWidth } from "./utils/layout-util";
-const Wrapper = styled.section`
-    height: 100vh;
-    width: 100%;
-    margin: 0 auto !important;
-    // background: linear-gradient(180deg, #9397ff, #aeb1ff 30%, #fff 75%, #fff);
-    background: linear-gradient(180deg, #c4c8ff, #d9dbff 30%, #fff 75%, #fff);
-    ${getResponsiveMaxWidth()};
-`;
+import Layout from "./layout";
+
 function App() {
     return (
-        <Wrapper>
+        <Layout>
             <Router />
-        </Wrapper>
+        </Layout>
     );
 }
 
