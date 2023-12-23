ed579cbd23a7cc2148e7f81d30b3e790eccc4fb8
luke7231 <tmddnjs7231@gmail.com>
2023-12-22 17:58:19 -0800

    feat: amplitude install

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import Router from "./router";
 import Layout from "./layout";
+import * as amplitude from "@amplitude/analytics-browser";
+
+amplitude.init("7b2ef4fff198401428b4a5ead249d79a");
 
 function App() {
     return (
